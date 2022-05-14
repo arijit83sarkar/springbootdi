a29d23d699c9c2062c12642c6a82185bd67cb47e
arijit83sarkar <mail2arijit83@gmail.com>
2022-05-15 00:53:00 +0530

    bean modified

--- src/main/java/com/raven/springbootdi/AstonMartin.java
+++ src/main/java/com/raven/springbootdi/AstonMartin.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class AstonMartin implements ICar {
 
-    private EngineServiceImpl engine;
+    private IEngineService engine;
 
     @Autowired
     public AstonMartin(EngineServiceImpl engine) {
